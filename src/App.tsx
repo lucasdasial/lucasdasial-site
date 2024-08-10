139d527cc4c7d3123c3f461b33e529aaba3b5898
Lucas Alves <as.lucasalves@gmail.com>
2024-08-10 01:17:46 -0400

    feat: create base rotate animation for texts

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="box">
+        <p>Tenho um forte interesse e experiência em:</p>
+        <div className="words">
+          <span>Apps mobile</span>
+          <span>Desenvolver APIs e integrações</span>
+          <span>Software as services</span>
+          <span>Cloud computing</span>
+          {/* <span>Cloud computing</span> */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
