f20da3b05b6f41db1d1eb5f2c00420e4e5379292
Lucas Alves <as.lucasalves@gmail.com>
2024-08-13 20:48:07 -0400

    feat: create intro hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,80 @@
-import './App.css'
+
+
+import './App.css';
 
 function App() {
 
-
   return (
-    <>
-      <div className="box">
-        <p className='strong_points_label'>Tenho um forte interesse e experiência em:</p>
+
+    <div className='page'>
+      <main className='main'>
+        <section className="content main_content">
+          <aside className='aside' data-aos="fade-left" >
+            <h1>I make <br /> mobile <br /> & web <br />apps.</h1>
+          </aside>
+
+          <section className='intro'>
+            <div className='intro_header' data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="400">
+              <img className='avatar' src="https://avatars.githubusercontent.com/u/72477227?v=4" alt="foto de perfil" />
+              <p className='title highlight'>Fullstack <br /> <span >developer</span>.</p>
+            </div>
+            <p className='intro description' data-aos="fade-up" data-aos-delay="800" >Olá, meu nome é Lucas Dasial. Sou um desenvolvedor Fullstack com ampla experiência no desenvolvimento e criação de aplicativos móveis, além de atuar na elaboração de APIs para integração entre sistemas e aplicações web.</p>
+            <nav data-aos="fade-up" data-aos-delay="1000">
+
+              {/* <button>
+                trabalho
+              </button>
+              <button>
+                social
+              </button> */}
+              <button>
+                ver mais
+              </button>
+
+            </nav>
+
+
+          </section>
+        </section>
+      </main>
+
+
+
+
+
+
+
+
+      {/* <p className='small-description'>Diariamente utilizo tecnologia para solucionar problemas, transformando em aplicações e produtos, sempre visando o valor agregado e em uma boa experiencia para os clientes nos projetos que atuo.</p> */}
+
+      {/* 
+
+      <div className='titles'>
+
+        <p> Diariamente utilizo tecnologia e programação para solucionar problemas, transformando em aplicações e produtos, sempre visando no valor agregado, na experiencia do usuário, em como impacta as outras pessoas
+          nos projetos que atuo.</p>
+      </div> */}
+
+
+      {/* <div className="strong-points-box">
+
+        <p className='small-description'>
+          Desenvolvedor Fullstack experiente, em entregar soluções completas, desde o conceito inicial até a implementação final.
+        </p>
+        <p className='strong_label'>Tenho um forte experiência e interesse em:</p>
         <div className="words">
-          <span className='apps'>Apps mobile</span>
+          <span className='cloud'>Cloud computing</span>
           <span className='api'> Criação de APIs</span>
           <span className='saas'>Testes automatizados</span>
-          <span className='cloud'>Cloud computing</span>
-          {/* <span>Cloud computing</span> */}
+          <span className='apps'>Aplicativos mobile</span>
         </div>
-      </div>
-    </>
+
+      </div> */}
+
+
+
+    </div>
+
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+AOS.init({
+  duration: 800
+})
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
