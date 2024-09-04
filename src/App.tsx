6dfa05a902f44a888286eff918127fd8960fc4f8
Lucas Alves <as.lucasalves@gmail.com>
2024-09-04 11:15:42 -0400

    update home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import "./App.css";
-import NavigationBar from "./components/navigationBar/navigationBar";
+import Welcome from "./components/welcome/welcome";
 
 function App() {
   return (
-    <div className="app-view">
-      <NavigationBar />
-    </div>
+    <>
+      <div className="app-view">
+        <header className="header_container">
+          <div className="header">
+            <h1>.dasial</h1>
+
+            <nav className="nav">
+              <a href="">Projetos</a>
+
+              <a href="">Trabalho</a>
+              <a href="">Contato</a>
+            </nav>
+          </div>
+        </header>
+        <Welcome />
+      </div>
+    </>
   );
 }
 
--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -1,27 +1,37 @@
 import styles from "./welcome.module.css";
+
 const Welcome = () => {
   return (
-    <section className={styles.container + " max-viewer"} id="home">
-      <div className={styles.intro}>
-        <h1>
+    <div className={styles.container}>
+      <div className={styles.text_box}>
+        <div className={styles.me} data-aos="fade-up">
+          <img src="images/me3.png" alt="Lucas" />
+          <p>
+            Lucas Alves
+            <br />
+            Full stack sw engineer
+          </p>
+        </div>
+        <h1 data-aos="fade-left">
           Desenvolvo{" "}
           <span className={styles.gradient}>soluções de software</span>,
           sistemas web e aplicativos.
         </h1>
-        <div className={styles.points}>
-          <p>Consultor e desenvolvedor na @IdopterLabs.</p>
+
+        <div className={styles.description} data-aos="fade-left">
+          <p>Consultor, desenvolvedor e freelancer.</p>
           <p>Graduado em ADS universidade da na Amazonia.</p>
           <p>Pós em engenharia de software na USP.</p>
         </div>
-        <a href="#next">Ver mais</a>
-      </div>
 
-      <div className={styles.illustration}>
-        <div className={styles.img_bg}>
-          <img src="images/1.svg" alt="ilustração" />
-        </div>
+        <button className={styles.cta} data-aos="fade-down">
+          Ver mais
+        </button>
       </div>
-    </section>
+      <aside className={styles.illustration_box} data-aos="fade-right">
+        <img className={styles.svg} src="images/1.png" alt="ilustração" />
+      </aside>
+    </div>
   );
 };
 export default Welcome;
