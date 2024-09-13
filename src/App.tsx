3a571d3ee033aa8acb290b384bb705cbf80f4da9
Lucas Alves <as.lucasalves@gmail.com>
2024-09-12 22:24:25 -0400

    feat: update bio

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,62 @@
-import "./App.css";
-import Projects from "./components/projects";
-import TopBar from "./components/topBar";
-import Welcome from "./components/welcome/welcome";
-
 function App() {
   return (
-    <div className="app-view">
-      <TopBar />
-      <Welcome />
-      <Projects />
-    </div>
+    <main className="app">
+      <aside className="aside aside-left">
+        <div className="content">
+          <img
+            className="profile_img"
+            src="fav.png"
+            alt="Foto de perfil Lucas Dasial"
+          />
+          <h1 className="about-title">
+            Olá, sou Lucas <br /> Dasial.
+          </h1>
+          <article className="about">
+            <p>
+              Eu sou Lucas Alves, também conhecido como Dasial, sou um
+              programador com 4 anos de experiência, graduado em Análise e
+              Desenvolvimento de Sistemas e especializado em Engenharia de
+              Software. Tenho paixão por desenvolver soluções completas e
+              eficientes, com foco em backend na construção de APIs para
+              integrações de sistemas, além do desenvolvimento de aplicativos
+              para Android e iOS.
+              <br />
+            </p>
+            <p>
+              Atualmente, faço parte da equipe da{" "}
+              <a href="https://en.idopterlabs.com.br/" target="blank">
+                @idopterlabs
+              </a>
+              , uma consultoria multinacional, onde atuo no desenvolvimento de
+              uma fintech líder na região Nordeste do Brasil.
+            </p>
+          </article>
+        </div>
+        <div className="links">
+          <p>
+            <a href="https://www.linkedin.com/in/lucasdasial/" target="blank">
+              Linkedin
+            </a>
+          </p>
+          <p>
+            <a href="https://github.com/lucasdasial" target="blank">
+              Github
+            </a>
+          </p>
+          <p>
+            <a href="https://www.instagram.com/dasial.dev/" target="blank">
+              Instagram
+            </a>
+          </p>
+          <p>
+            <a href="https://dev.to/lucasdasial" target="blank">
+              Blog
+            </a>
+          </p>
+        </div>
+      </aside>
+      {/* <aside className="aside assise-right"></aside> */}
+    </main>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './theme/root.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./global.css";
 
 AOS.init({
-  duration: 800
-})
+  duration: 800,
+});
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
